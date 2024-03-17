1cc7e5d5c941a6d00393896a62eb3f2ad88d148f
aidyn <aidyn.meiramkhanulu@gmail.com>
2024-03-17 18:23:08 +0500

    clear app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,5 @@
-import { useEffect, useState } from "react";
-import { baseApi } from "./api";
-
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    baseApi("GET")
-      .then((result) => {
-        // Обработка успешного результата
-        console.log("Результат запроса:", result);
-        setData(result);
-      })
-      .catch((error) => {
-        // Обработка ошибки
-        console.error("Ошибка запроса:", error.message);
-      });
-  }, []);
-
-  return (
-    <>
-      <div>
-        ErpSystem
-        {Array.isArray(data) &&
-          data.map((item) => {
-            return <div key={item.id}>{item.title}</div>;
-          })}
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
