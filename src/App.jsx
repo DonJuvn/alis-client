879ccc369c42198271f5dfd5246796308efc70ee
aidyn <aidyn.meiramkhanulu@gmail.com>
2024-03-18 21:11:06 +0500

    update App.jsx
    *add routerProvider with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/index';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
