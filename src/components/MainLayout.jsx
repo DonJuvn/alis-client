5484d41d4edcc7e070f38a315c031f0fbc314ce1
aidyn <aidyn.meiramkhanulu@gmail.com>
2024-03-18 21:16:14 +0500

    Add MainLayout
    *add Header
    *add Sidebar

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,28 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Box, CircularProgress, Stack, Toolbar } from '@mui/material';
+import { Sidebar } from './Sidebar/Sidebar';
+import { Header } from './Header/Header';
+import { Breadcrumbs } from '../utils/BreadCrumbs';
+
+export const MainLayout = () => (
+  // <SidebarProvider>
+  <>
+    <Header />
+    <Suspense fallback={<CircularProgress />}>
+      <Box>
+        <Stack direction="row">
+          <Sidebar />
+          <Box flex={1}>
+            <Toolbar />
+            <Box m={2}>
+              <Breadcrumbs />
+              <Outlet />
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
+    </Suspense>
+  </>
+  // </SidebarProvider>
+);
