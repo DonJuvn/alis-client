0649692e868600bddc41603ffd9fea252b9ea023
aidyn <aidyn.meiramkhanulu@gmail.com>
2024-03-27 16:51:19 +0500

    add theme
    *create theme component
    *add theme context to component
    *add theme provider to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/Routes';
+import { ThemeProvider } from './utils/Theme';
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
