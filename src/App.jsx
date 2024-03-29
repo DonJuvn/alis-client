6b8c46dd157a6b6c37b21104c4c57968881fa559
aidyn <aidyn.meiramkhanulu@gmail.com>
2024-03-30 00:54:45 +0500

    add cssBaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/Routes';
+import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <RouterProvider router={router} />
     </>
   );
