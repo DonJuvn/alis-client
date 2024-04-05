fb130b0b2f5eb1ec034e57e85eb400c4f78a2aaa
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-05 16:44:32 +0600

    add admin to routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,7 @@ import { NotFound } from '../utils/NotFound.jsx';
 import { ErrorPage } from '../utils/ErrorPage.jsx';
 import { Editor } from '../components/Editor/Editor.jsx';
 import { Settings } from '../components/Settings/Settings.jsx';
+import { Admin } from '../components/Admin/Admin.jsx';
 
 export const router = createBrowserRouter([
   {
@@ -35,6 +36,11 @@ export const router = createBrowserRouter([
               {
                 index: true,
                 element: <Settings />,
+              },
+              {
+                path: '/settings/admin',
+                index: true,
+                element: <Admin />,
               },
             ],
           },
