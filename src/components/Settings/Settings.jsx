bba3c5dcace6a215830a526f5f14cd26bdd3a07c
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-17 00:23:59 +0500

    some fixups

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,22 +1,20 @@
-import { Box, Paper, Switch } from '@mui/material';
-import { useTheme } from '../../utils/Theme';
+import { Paper, Switch } from '@mui/material';
+import { useTheme } from '@utils/Theme';
 
 export const Settings = () => {
   const { theme, handleThemeChange } = useTheme();
   return (
-    <Box flexDirection="row">
-      <Paper
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <p>Dark Theme</p>
-        <Switch
-          onChange={handleThemeChange}
-          defaultChecked={theme.palette.mode == 'dark'}
-        />
-      </Paper>
-    </Box>
+    <Paper
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <p>Dark Theme</p>
+      <Switch
+        onChange={handleThemeChange}
+        defaultChecked={theme.palette.mode == 'dark'}
+      />
+    </Paper>
   );
 };
