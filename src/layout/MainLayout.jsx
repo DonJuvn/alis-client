de3dfe00234b29f4908524f25dd81459d33ce341
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-05-14 18:19:51 +0500

    fix adaptivity

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -7,10 +7,10 @@ import MainContent from './MainContent';
 export const MainLayout = () => {
   return (
     <Suspense fallback={<CircularProgress />}>
-      <Box height="100%">
+      <Box sx={{ overflow: 'hidden' }} height="100%" width="99%">
         <Stack direction="row" width="100%" height="95vh" m={2}>
           <Sidebar />
-          <Box mx={2} sx={{ minWidth: '100%' }}>
+          <Box mx={2} sx={{ width: '100%' }}>
             <MainContent />
           </Box>
         </Stack>
