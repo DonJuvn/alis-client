3008b6dff2f8d9231cc2d2a65b955ba68b8c5bb2
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-05-14 18:02:51 +0500

    add mapping inputs

--- src/components/MappingInputs.jsx
+++ src/components/MappingInputs.jsx
@@ -1,5 +1,17 @@
 const MappingInputs = () => {
-  return <div>MappingInputs</div>;
+  return (
+    <div className="mapping-inputs">
+      <h3>Inputs</h3>
+      <label htmlFor="name">Name</label>
+      <input id="name" type="text" value="name" />
+      <label htmlFor="name">Email</label>
+      <input id="email" type="text" value="Email" />
+      <label htmlFor="Header">Header</label>
+      <input id="header" type="text" value="Header" />
+      <label htmlFor="title">Title</label>
+      <input id="title" type="text" value="title" />
+    </div>
+  );
 };
 
 export default MappingInputs;
